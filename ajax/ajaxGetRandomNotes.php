8df0935c2c8e8bf1fcb32153d97a978b1b1f4611
joshua.cook <joshua.cook@webappuk.com>
2016-09-09 16:16:32 +0100

    Ajax requests

--- ajax/ajaxGetRandomNotes.php
+++ ajax/ajaxGetRandomNotes.php
@@ -1,19 +1,19 @@
 <?php
 //This gets the random notes for the person generated in the passport
-$.ajax({
-    url: "",
-    data: {
-        item1:'Surname',
-        item2:'Names',
-        item3:'Gender',
-        item4:'Race',
-        item5:'otherNotes'
-    },
-    success: function(response){
-        $('#notesfield').html(response) //probably print to notes
-    }
-    error: function(response){
-        alert('ajax is being poo');
-    }
-});
+    $.ajax({
+        url: "",
+        data: {
+            item1:'Surname',
+            item2:'Names',
+            item3:'Gender',
+            item4:'Race',
+            item5:'otherNotes'
+        },
+        success: function(data){
+            $('#notesfield').html(data) //probably print to notes
+        }
+        error: function(data){
+            alert('ajax is being poo');
+        }
+    });
 ?>
--- ajax/ajaxGetRandomPerson.php
+++ ajax/ajaxGetRandomPerson.php
@@ -1,15 +1,15 @@
 <?php
 //This gets the random Person generated for the passport
-$.ajax({
-    url: "",
-    data: {
-        item1:'Image'
-    },
-    success: function(response){
-        $('#imageField').html(response) //probably print to notes
-    }
-    error: function(response){
-        alert('ajax is being poo');
-    }
-});
+    $.ajax({
+        url: "",
+        data: {
+            item1:'Image'
+        },
+        success: function(data){
+            $('#imageField').html(data) //probably print to notes
+        }
+        error: function(data){
+            alert('ajax is being poo');
+        }
+    });
 ?>
