4bfdf2e098e6b2522a7536b124ea54e7bae2b46f
Lee Flaherty <Lee.Flaherty@webappuk.com>
2016-09-09 14:41:31 +0100

    Updated spPersonGet and added more data
    Comments added throughout spPersonGet and also added issueDate and ExpiryDate fields.
    Also changed first name and last name into two seperate tables, this will allow for a larger variety of names available

--- sql/ticketsplease.sql
+++ sql/ticketsplease.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 09, 2016 at 02:22 PM
+-- Generation Time: Sep 09, 2016 at 03:37 PM
 -- Server version: 10.1.16-MariaDB
 -- PHP Version: 5.6.24
 
@@ -26,38 +26,52 @@ DELIMITER $$
 --
 CREATE DEFINER=`root`@`localhost` PROCEDURE `spPersonGet` ()  NO SQL
 BEGIN
-  DECLARE 	p_nameRand 		INT;
-  DECLARE   p_natRand 		INT;
-  DECLARE   p_skinRand 		INT;
-  DECLARE	p_note			varchar(520) DEFAULT '';
-  DECLARE   p_noteRand 		INT;
-  DECLARE   p_noteCount		INT;
-  DECLARE 	p_noteAmount 	INT;
-  DECLARE   p1				INT DEFAULT 0;
+	DECLARE 	p_nameRand 		INT;
+    DECLARE 	p_lastnameRand	INT;
+	DECLARE  	p_natRand 		INT;
+	DECLARE  	p_skinRand 		INT;
+	DECLARE		p_note			varchar(520) DEFAULT '';
+	DECLARE   	p_noteRand 		INT;
+	DECLARE   	p_noteCount		INT;
+	DECLARE 	p_noteAmount 	INT;
+	DECLARE   	p1				INT DEFAULT 0;
+	DECLARE		issueDate		DateTime;
+	DECLARE   	expiryDate		DateTime;
   
-  SET p_nameRand = (select floor(1+rand()*count(*)) from name);
-  SET p_natRand = (select floor(1+rand()*count(*)) from nationality);
-  SET p_skinRand = (select floor(1+rand()*count(*)) from skintone);
-  SET p_noteRand = (select floor(1+rand()*count(*)) from notes);
-  SET p_noteCount = (select count(*) from notes);
-  SET p_noteAmount = (select floor(1+RAND()*5));
+    #Select random ID's that will correspond to random data to return
+	SET p_nameRand = (select floor(1+rand()*count(*)) from name);
+    SET p_lastnameRand = (select floor(1+rand()*count(*)) from lastname);
+	SET p_natRand = (select floor(1+rand()*count(*)) from nationality);
+	SET p_skinRand = (select floor(1+rand()*count(*)) from skintone);
+	SET p_noteRand = (select floor(1+rand()*count(*)) from notes);
+    
+    #get the number of notes and randomly select the number of notes we want.
+	SET p_noteCount = (select count(*) from notes);
+	SET p_noteAmount = (select floor(1+RAND()*5));
+    
+    #Get a random date for the issue date and set expiration date 5 years after the issue date
+	SET issueDate = TIMESTAMPADD(SECOND, FLOOR(RAND() * TIMESTAMPDIFF(SECOND, '2011-01-01 00:00:00', '2016-12-31 00:00:00')), '2011-01-01 00:00:00');
+	SET expiryDate = DATE_ADD(issueDate, INTERVAL 5 YEAR);
   
-  concatNotes: LOOP
-	SET p1 = p1 + 1;
-    if p1 < p_noteAmount THEN
-		SET p_noteRand = (select floor(1+rand()*p_noteCount));
-		SET p_note = CONCAT(p_note, (select Notes from notes where ID = p_noteRand));
-        SET p_note = CONCAT(p_note, ':');
-		ITERATE concatNotes;
-	END IF;
-	LEAVE concatNotes;
-  END LOOP concatNotes;
+    #Concatonate 'n' number of random notes together, where n is the random number of notes we want given before.
+	concatNotes: LOOP
+		SET p1 = p1 + 1;
+		if p1 < p_noteAmount THEN
+			SET p_noteRand = (select floor(1+rand()*p_noteCount));
+			SET p_note = CONCAT(p_note, (select Notes from notes where ID = p_noteRand));
+			SET p_note = CONCAT(p_note, ':');
+			ITERATE concatNotes;
+		END IF;
+		LEAVE concatNotes;
+	END LOOP concatNotes;
   
-  select nam.Name, nam.Gender, nat.Country, st.SkinTone, p_note
-  from name nam, Nationality nat, skintone st 
-  where nam.ID = p_nameRand 
-  AND nat.ID = p_natRand
-  AND st.ID = p_skinRand;
+	#Select all the data from the randomly generated ID's
+	select CONCAT(nam.Name,CONCAT(' ',lst.name)) as Name, nam.Gender, nat.Country, st.SkinTone, p_note, issueDate, expiryDate
+	from name nam, Nationality nat, skintone st, lastname lst 
+	where nam.ID = p_nameRand 
+	AND nat.ID = p_natRand
+	AND st.ID = p_skinRand
+    AND	lst.ID = p_lastnameRand;
 END$$
 
 CREATE DEFINER=`root`@`localhost` PROCEDURE `spPersonInfoGet` (IN `@PersonID` INT)  NO SQL
@@ -93,6 +107,46 @@ INSERT INTO `hairstyle` (`ID`, `Colour`, `style`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `lastname`
+--
+
+CREATE TABLE `lastname` (
+  `ID` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `lastname`
+--
+
+INSERT INTO `lastname` (`ID`, `name`) VALUES
+(1, 'Flaherty'),
+(2, 'Boorer'),
+(3, 'Cook'),
+(4, 'Johnson'),
+(5, 'Smith'),
+(6, 'Williams'),
+(7, 'Jones'),
+(8, 'Brown'),
+(9, 'Davis'),
+(10, 'Miller'),
+(11, 'Wilson'),
+(12, 'Moore'),
+(13, 'Taylor'),
+(14, 'Johnson'),
+(15, 'Smith'),
+(16, 'Williams'),
+(17, 'Jones'),
+(18, 'Brown'),
+(19, 'Davis'),
+(20, 'Miller'),
+(21, 'Wilson'),
+(22, 'Moore'),
+(23, 'Taylor');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `login`
 --
 
@@ -121,9 +175,18 @@ CREATE TABLE `name` (
 --
 
 INSERT INTO `name` (`Name`, `Gender`, `ID`) VALUES
-('Lee Flaherty', 'Male', 1),
-('Joshua Cook', 'Male', 2),
-('Aubrey Boorer', 'Male', 3);
+('Lee', 'Male', 1),
+('Joshua', 'Male', 2),
+('Aubrey', 'Male', 3),
+('James', 'Male', 4),
+('Mary', 'Female', 5),
+('John', 'Male', 6),
+('Patricia', 'Female', 7),
+('Robert', 'Male', 8),
+('Linda', 'Female', 9),
+('Michael', 'Male', 10),
+('Barbara', 'Female', 11),
+('William', 'Male', 12);
 
 -- --------------------------------------------------------
 
@@ -238,6 +301,12 @@ ALTER TABLE `hairstyle`
   ADD PRIMARY KEY (`ID`);
 
 --
+-- Indexes for table `lastname`
+--
+ALTER TABLE `lastname`
+  ADD PRIMARY KEY (`ID`);
+
+--
 -- Indexes for table `login`
 --
 ALTER TABLE `login`
@@ -283,6 +352,11 @@ ALTER TABLE `skintone`
 ALTER TABLE `hairstyle`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
+-- AUTO_INCREMENT for table `lastname`
+--
+ALTER TABLE `lastname`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+--
 -- AUTO_INCREMENT for table `login`
 --
 ALTER TABLE `login`
@@ -291,7 +365,7 @@ ALTER TABLE `login`
 -- AUTO_INCREMENT for table `name`
 --
 ALTER TABLE `name`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 --
 -- AUTO_INCREMENT for table `nationality`
 --
