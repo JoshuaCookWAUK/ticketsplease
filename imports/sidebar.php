5058ebeb0272bc204e2d3a07599118bea85c1e9c
Aubrey Boorer <aubrey.boorer@webappuk.com>
2016-09-09 11:20:07 +0100

    First Commit
    The initial commit of the foundation for our project.

---
+++ imports/sidebar.php
@@ -0,0 +1,19 @@
+<sidebar>
+    <sidebar-header>
+        <h3>Tickets Please!</h3>
+    </sidebar-header>
+    <sidebar-group>
+        <sidebar-title>
+            <h3>Passports</h3>
+        </sidebar-title>
+        <sidebar-item data-icon="flag-gb">
+            <p>British</p>
+        </sidebar-item>
+        <sidebar-item data-icon="flag-fr">
+            <p>French</p>
+        </sidebar-item>
+        <sidebar-item data-icon="flag-de">
+            <p>German</p>
+        </sidebar-item>
+    </sidebar-group>
+</sidebar>
--- index.php
+++ index.php
@@ -1,9 +1,14 @@
 <!DOCTYPE html>
 <html>
-  <head>
-  
-  </head>
-  <body>
-  
-  </body>
+    <head>
+        <meta charset="utf-8" />
+        <?php include "imports/references.php"; ?>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+        <script src="js/core.js"></script>
+        <title>Game</title>
+    </head>
+    <body>
+        <?php include "imports/sidebar.php"; ?>
+        <canvas id="canvas"></canvas>
+    </body>
 </html>
