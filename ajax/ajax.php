b2d1454bed0fc5664ad6204490f0a4e9346e7bf8
AubreyBoorerWAUK <aubrey.boorer@webapplicationsuk.com>
2016-09-09 11:00:21 +0100

    Initial commit of example ajax page.

---
+++ ajax/ajax.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  
+  </head>
+  <body>
+    <p>Some AJAX page.</p>
+    <p>These should all be stored in the AJAX folder and given useful names like ajaxGetRandomTicket.php or something.</p>
+  </body>
+</html>
