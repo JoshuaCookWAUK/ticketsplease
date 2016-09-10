63ff21ee8b046e079c3cbc300471ae2a3515dcf0
Aubrey Boorer <aubrey.boorer@webappuk.com>
2016-09-10 11:47:00 +0100

    changed to core.js

--- js/core.js
+++ js/core.js
@@ -2,28 +2,4 @@ var canvasHandles = new Array();
 $(document).ready(function() {
 	canvasHandles.push({name: "menu", handle: new CanvasMenu()});
 	console.log(canvasHandles[0].handle.stop());
-	var wheresMeMouseAt = (function () {
-		document.addEventListener('mousemove', mouseHandler);
-		let location = {
-			x : 0,
-			y : 0
-		};
-		function mouseHandler (e) {
-			location = {
-				x : e.clientX,
-				y : e.clientY
-			}
-		}
-		function getLocayshun () {
-			return location
-		}
-		return location;
-	})();
-
-	console.log(wheresMeMouseAt);
-
-	setTimeout(function () {
-		console.log(wheresMeMouseAt)
-	}, 1000)
-
 });
