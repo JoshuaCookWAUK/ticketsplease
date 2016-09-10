42a9e4d173692226dc9904bb7e3bc5d3a769b8c5
Aubrey Boorer <aubrey.boorer@webappuk.com>
2016-09-10 11:52:06 +0100

    fixed

--- imports/sidebar.game.php
+++ imports/sidebar.game.php
@@ -1,38 +1,47 @@
 <?php
     include "../php/credentials.php";
-    $conn = getConnection();
-    $validPassports = mysqli_query($conn, "SELECT * FROM nationality");
-    $validTickets = mysqli_query($conn, "SELECT * FROM supplier");
-    mysqli_close($conn);
-
- ?>
-
+    $validPassportCONN = getConnection();
+    $validPassportCONN->query("SET @param  = 0");
+    $validPassports = $validPassportCONN->query("CALL spValidNatGet(@param)");
+    $validTicketCONN = getConnection();
+    $validTicketCONN->query("SET @ticketAmount  = 0");
+    $validTickets = $validTicketCONN->query("CALL spValidTicketGet(@ticketAmount)");
+?>
 <sidebar-header>
     <h3>Tickets Please!</h3>
 </sidebar-header>
 <sidebar-inner id="sidebar-game">
 <sidebar-group id="sidebar-group-main">
-        <sidebar-item data-icon="arrow-left">
+        <sidebar-item id="main-menu" data-icon="arrow-left">
             <p>Main Menu</p>
         </sidebar-item>
     </sidebar-group>
     <sidebar-break></sidebar-break>
-    <sidebar-title>
-        <h3>Valid Passports</h3>
-    </sidebar-title>
+    <sidebar-group id="sidebar-group-buttons">
+        <sidebar-item id="accept-button" data-icon="accept">
+            <p>Accept</p>
+        </sidebar-item>
+        <sidebar-item id="decline-button" data-icon="decline">
+            <p>Decline</p>
+        </sidebar-item>
+    </sidebar-group>
+    <sidebar-break></sidebar-break>
     <sidebar-group id="sidebar-group-passports">
+        <sidebar-title>
+            <h3>Valid Passports</h3>
+        </sidebar-title>
         <?php while($queryData = $validPassports->fetch_assoc()): ?>
             <sidebar-item id="passport-<?= $queryData["RegionCode"] ?>" data-icon="flag-<?= $queryData["RegionCode"] ?>">
                 <p><?= $queryData["Country"] ?></p>
             </sidebar-item>
         <?php endwhile; ?>
     </sidebar-group>
-    <sidebar-title>
-        <h3>Valid Tickets</h3>
-    </sidebar-title>
-    <sidebar-group id="sidebar-group-passports">
+    <sidebar-group id="sidebar-group-ticket">
+        <sidebar-title>
+            <h3>Valid Tickets</h3>
+        </sidebar-title>
         <?php while($queryData = $validTickets->fetch_assoc()): ?>
-            <sidebar-item id="new-game" data-icon="flag-<?= $queryData["RegionCode"] ?>">
+            <sidebar-item id="validTicket-<?= $queryData["RegionCode"] ?>" data-icon="flag-<?= $queryData["RegionCode"] ?>">
                 <p><?= $queryData["Name"] ?></p>
             </sidebar-item>
         <?php endwhile; ?>
--- js/core.js
+++ js/core.js
@@ -1,5 +1,5 @@
 var canvasHandles = new Array();
 $(document).ready(function() {
+	switchSidebar('sidebar');
 	canvasHandles.push({name: "menu", handle: new CanvasMenu()});
-	console.log(canvasHandles[0].handle.stop());
 });
